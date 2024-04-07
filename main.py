5b7d14a7c95aa52354a0da9eba1eb5b8f0f16a55
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-04-07 21:45:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def predict_sales(request: PredictionRequest):
     return {"predictions": Y_pred.tolist()}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
